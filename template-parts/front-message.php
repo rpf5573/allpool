7dcec7e47162d2af1904fdd2f230f5beed4db254
YOONBYEONGIN <rpf5573@gmail.com>
2018-07-19 17:20:11 +0900

    making points system

--- template-parts/front-message.php
+++ template-parts/front-message.php
@@ -1,3 +1,3 @@
 <h1 class="front-message">
-  알풀에서 <span class="red d-ib q-icon">질문<img src="<?php echo ANSPRESS_URL . "assets/images/q-icon.png"; ?>"></span> 올리고 <span class="blue d-ib a-icon">점심값<img src="<?php echo ANSPRESS_URL . "assets/images/a-icon.png"; ?>"></span> 벌어가세요!
+  알풀에서 <span class="red d-ib q-icon">답변<img src="<?php echo ANSPRESS_URL . "assets/images/a-icon.png"; ?>"></span> 달고 <span class="blue d-ib a-icon">점심값<img src="<?php echo ANSPRESS_URL . "assets/images/money.png"; ?>"></span> 벌어가세요!
 </h1>
